59ca924cb68421624371bb4df99364afa3c143e4
Chakkaphan16889 <chongphimai@gmail.com>
2025-03-13 19:42:37 +0700

    first Commit

---
+++ src/app/(routeapp)/contact/page.tsx
@@ -0,0 +1,10 @@
+import Contact from '@/compoments/commoments/Contact'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <Contact></Contact>
+    </div>
+  )
+}
